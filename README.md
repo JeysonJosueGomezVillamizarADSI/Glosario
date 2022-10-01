7d8a8afec736ede4cfda7f2a86bb5cbd20015f36
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-10-01 13:27:08 -0500

    Clausula

--- README.md
+++ README.md
@@ -127,4 +127,25 @@ function comer (){
 console.log(otraFruta)
 ```
 
- 
+#  Clausula
+
+Una cláusula es una función que tiene acceso al ámbito de su función padre, incluso después de que la función padre haya terminado de ejecutar.
+
+```javascript
+const miFuncion = () => {
+     let miValor = 2;
+     console.log(miValor);
+
+     const funcionHija = () => {
+          console.log(miValor += 1);
+     }
+
+     return funcionHija;
+}
+
+const resultado = miFuncion();
+console.log(resultado);
+resultado();
+resultado();
+resultado();
+```
--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 addEventListener('DOMContentLoaded', (e) => {
 
-    let otraFruta='Pera'
-
-    function comer (){
-        let fruta='Manzana';
-        console.log("Estoy comiendo una " + fruta)
-    }
-    console.log(otraFruta)
+    function iniciar() {
+        var nombre = "Jeyson";  // La variable nombre es una variable local creada por iniciar.
+        function mostrarNombre() {  // La función mostrarNombre es una función interna, una clausura.
+          alert(nombre);  // Usa una variable declarada en la función externa.
+        }
+        mostrarNombre();
+      }
+      iniciar();
 
 });
